abee88098c59bca50f31a802c7c4ca7d31616c5e
Daviti Kharchilava <kharcho10@gmail.com>
2020-01-12 19:44:28 +0400

    Create a React app and publish it to GitHub Pages

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
 import React from "react";
-import logo from "./logo.svg";
 import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <p className="Person">ae ra bazaria</p>
+    <div>
+      <p>ae ra bazaria</p>
     </div>
   );
 }
