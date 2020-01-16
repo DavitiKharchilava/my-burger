20b0c846b730bb35032413c25cf4322d1eba48fd
Daviti Kharchilava <kharcho10@gmail.com>
2020-01-17 01:09:26 +0400

    First commit

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
-import React from "react";
-import "./App.css";
+import React, { Component } from "react";
+import Layout from "./components/Layout/Layout";
+import BurgerBuilder from "./containers/BurgerBuilder/BurgerBuilder";
 
-function App() {
-  return (
-    <div>
-      <p>ae ra bazaria</p>
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <Layout>
+          <BurgerBuilder/>
+        </Layout>
+      </div>
+    );
+  }
 }
 
 export default App;
