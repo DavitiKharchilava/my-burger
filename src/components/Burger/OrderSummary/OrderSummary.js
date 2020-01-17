507f2a757df13327a2c78185039995adabb34576
Daviti Kharchilava <kharcho10@gmail.com>
2020-01-17 23:21:02 +0400

    First commit

--- src/components/Burger/OrderSummary/OrderSummary.js
+++ src/components/Burger/OrderSummary/OrderSummary.js
@@ -19,7 +19,7 @@ const OrderSummary = props => {
       <p>
         <strong>Total Price: {props.price.toFixed(2)}</strong>
       </p>
-      <p>Comtinue to Checkout ?</p>
+      <p>Continue to Checkout ?</p>
       <Button btnType="Danger" clicked={props.purchaseCancelled}>
         CANCEL
       </Button>
