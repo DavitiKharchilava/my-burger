14664b5637c484f7b95f457aa1dae114befa3fbb
Daviti Kharchilava <kharcho10@gmail.com>
2020-02-27 19:16:32 +0400

    First commit

--- src/store/actions/burgerBuilder.js
+++ src/store/actions/burgerBuilder.js
@@ -30,7 +30,7 @@ export const fetchIngredientsFailed = () => {
 
 export const initIngredients = () => {
     return dispatch => {
-        axios.get( 'https://react-my-burger-f50c0.firebaseio.com//ingredients.json' )
+        axios.get( 'https://react-my-burger-f50c0.firebaseio.com/ingredients.json' )
             .then( response => {
                dispatch(setIngredients(response.data));
             } )
